5df6e22431940f2c067009cd1c4463d1e076bf59
Peter Dudfield <peter.dudfield@hotmail.com>
2026-02-03 11:06:01 +0000

    small updates to get tests working

--- src/quartz_api/internal/models/__init__.py
+++ src/quartz_api/internal/models/__init__.py
@@ -1,6 +1,8 @@
 """Domain models and interfaces for the application."""
 
 from .db_interface import (
+    EnergyType,
+    LocationType,
     StorageInterface,
     StorageClientDependency,
     get_storage_client,
